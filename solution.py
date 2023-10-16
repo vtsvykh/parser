ab25f5822e535cbbd21f8b617d975ef0c9bec537
Igor <uchansansan@gmail.com>
2023-10-16 14:28:46 +0000

    Update solution.py
    add products url list

--- solution.py
+++ solution.py
@@ -1,22 +1,24 @@
 #артикул, наименование, бренд, цена, скидка %, страна производитель
 import requests
-
+import json
+from requests import Response
 f = open('output.txt', 'w')
 out = open('page.txt', 'w')
 search = input().strip()
 card_url = 'https://www.lamoda.ru/p/'
-payload = {'q': search, 'sort': 'price_asc'}
+payload = {'q': search, 'sort': 'price_asc', 'page': 1} 
 #url = f'https://www.lamoda.ru/catalogsearch/result/?q={input().replace(" ","+")}&sort=price_asc'
 url = 'https://www.lamoda.ru/catalogsearch/result'
 #носки зеленые мужские adidas
 
 response = requests.get(url, params=payload)
-print(response)
+print(type(response))
 page = response.text
 idx = page.find('"products"')
 new_page = page[idx:]
 idxe = new_page.find('"products_meta"')
 out.write(page[idx:idxe+idx])
+
 # print(page[idx:idxe+idx])
 len(page)
 # print(page[page.find('"pagination"'):])
@@ -26,37 +28,31 @@ print(pagitation_dict)
 f.write(pagitation_dict)
 page_c = page
 
-article_list = []
-while True:
-	if page.find("short_sku") != -1:
-		idx = page.find("short_sku")
-		sign = page.find('",')
-		article = page[idx:sign].split(':')
-		page = page[26:]
-		if article != [''] and article[1][1:] not in article_list:
-			article_list.append(article[1][1:])
-	else:
-		break
-print(article_list, len(article_list))
+
+num_pages = json.loads(pagitation_dict)['pages']
+
+def get_artikules(page):
+	article_list = []
+	while True:
+		if page.find("short_sku") != -1:
+			idx = page.find("short_sku")
+			sign = page.find('",')
+			article = page[idx:sign].split(':')
+			page = page[26:]
+			if article != [''] and article[1][1:] not in article_list:
+				article_list.append(article[1][1:])
+		else:
+			break
+	return article_list
 
 
+for page in range(num_pages):
+	response = requests.get(url, params=payload)
+	page = response.text
+	arlikules = get_artikules(page)
+	for arlikul in arlikules:
+		url_product = f'https://www.lamoda.ru/p/{arlikul}'
+		print(url_product)
+		#TODO: сюда добалять код чтобы доставать артикул, наименование, бренд, цена, скидка %, страна производитель
 
-
-
-'''
-divs = a('x-product-card-description__microdata-wrap')
-for div in divs:
-	print(div.text_content())
-
-
-print(url)
-'''
-# print(requests.get(url, params=payload).text)
-# print(requests.get(url, params=payload).text)
-# f.write(requests.get(url, params=payload).text)
-#"x-product-card__card x-product-card__card_catalog"
-#'x-product-card-description__microdata-wrap' цена
-#'_value_ajirn_27 ui-product-description-attribute-production_country' страна
-#артикул в ссылке
-#"x-product-card-description__price-new x-product-card-description__price-WEB8507_price_no_bold _price_1okc5_8"
 
